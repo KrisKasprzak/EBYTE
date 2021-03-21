a9800a577613990bba4c8f7489c26f73d04622cd
Kris Kasprzak <kris.kasprzak@yahoo.com>
2021-03-20 20:16:30 -0500

    Add files via upload

---
+++ Examples/ESP8266/Receive/Receive.ino
@@ -0,0 +1,128 @@
+/*
+
+  This example shows how to connect to an EBYTE transceiver
+  using an ESP8266
+
+  This code for for the sender
+
+
+  connections
+  Module      ESP6266
+  M0          GPIO14 (pin D5)
+  M1          GPIO5  (pin D6)
+  Rx          GPIO2  (pin D1)
+  Tx          GPIO3  (Pin D2)
+  Aux         GPIO6  (pin D0)
+  Vcc         3V3
+  Gnd         Gnd
+
+*/
+
+#include <SoftwareSerial.h>
+#include "EBYTE.h"
+
+
+/*
+WARNING: IF USING AN ESP8266
+DO NOT USE THE PIN NUMBERS PRINTED ON THE BOARD
+YOU MUST USE THE ACTUAL GPIO NUMBER
+*/
+#define PIN_RX 14   //D5 on the board
+#define PIN_TX 12   //D6 on the board
+
+#define PIN_M0 5    //D1 on the board
+#define PIN_M1 4    //D2 on the board
+#define PIN_AX 16   //D0 on the board
+
+
+// i recommend putting this code in a .h file and including it
+// from both the receiver and sender modules
+struct DATA {
+  unsigned long Count;
+  int Bits;
+  float Volts;
+  float Amps;
+
+};
+
+// these are just dummy variables, replace with your own
+int Chan;
+DATA MyData;
+unsigned long Last;
+
+// connect to any digital pin to connect to the serial port
+// don't use pin 01 and 1 as they are reserved for USB communications
+SoftwareSerial ESerial(PIN_RX, PIN_TX);
+
+// create the transceiver object, passing in the serial and pins
+EBYTE Transceiver(&ESerial, PIN_M0, PIN_M1, PIN_AX);
+
+void setup() {
+
+
+  Serial.begin(9600);
+
+  ESerial.begin(9600);
+  Serial.println("Starting Reader");
+
+  // this init will set the pinModes for you
+  Transceiver.init();
+
+  // all these calls are optional but shown to give examples of what you can do
+
+  // Serial.println(Transceiver.GetAirDataRate());
+  // Serial.println(Transceiver.GetChannel());
+  // Transceiver.SetAddressH(1);
+  // Transceiver.SetAddressL(1);
+  // Chan = 15;
+  // Transceiver.SetChannel(Chan);
+  // save the parameters to the unit,
+  // Transceiver.SetPullupMode(1);
+  // Transceiver.SaveParameters(PERMANENT);
+
+  // you can print all parameters and is good for debugging
+  // if your units will not communicate, print the parameters
+  // for both sender and receiver and make sure air rates, channel
+  // and address is the same
+  Transceiver.PrintParameters();
+
+
+}
+
+void loop() {
+
+  // if the transceiver serial is available, proces incoming data
+  // you can also use Transceiver.available()
+
+
+  if (ESerial.available()) {
+
+    // i highly suggest you send data using structures and not
+    // a parsed data--i've always had a hard time getting reliable data using
+    // a parsing method
+
+    Transceiver.GetStruct(&MyData, sizeof(MyData));
+
+    // note, you only really need this library to program these EBYTE units
+    // you can call readBytes directly on the EBYTE Serial object
+    // ESerial.readBytes((uint8_t*)& MyData, (uint8_t) sizeof(MyData));
+
+
+    // dump out what was just received
+    Serial.print("Count: "); Serial.println(MyData.Count);
+    Serial.print("Bits: "); Serial.println(MyData.Bits);
+    Serial.print("Volts: "); Serial.println(MyData.Volts);
+    // if you got data, update the checker
+    Last = millis();
+
+  }
+  else {
+    // if the time checker is over some prescribed amount
+    // let the user know there is no incoming data
+    if ((millis() - Last) > 1000) {
+      Serial.println("Searching: ");
+      Last = millis();
+    }
+
+  }
+}
---
+++ Examples/ESP8266/Send/Send.ino
@@ -0,0 +1,111 @@
+/*
+
+  This example shows how to connect to an EBYTE transceiver
+  using an ESP8266
+
+  This code for for the sender
+
+
+  connections
+  Module      ESP6266
+  M0          GPIO14 (pin D5)
+  M1          GPIO5  (pin D6)
+  Rx          GPIO2  (pin D1)
+  Tx          GPIO3  (Pin D2)
+  Aux         GPIO6  (pin D0)
+  Vcc         3V3
+  Gnd         Gnd
+
+*/
+
+#include <SoftwareSerial.h>
+#include "EBYTE.h"
+
+/*
+WARNING: IF USING AN ESP8266
+DO NOT USE THE PIN NUMBERS PRINTED ON THE BOARD
+YOU MUST USE THE ACTUAL GPIO NUMBER
+*/
+
+#define PIN_RX 14   //D5 on the board
+#define PIN_TX 12   //D6 on the board
+
+#define PIN_M0 5    //D1 on the board
+#define PIN_M1 4    //D2 on the board
+#define PIN_AX 16   //D0 on the board
+
+// i recommend putting this code in a .h file and including it
+// from both the receiver and sender modules
+
+// these are just dummy variables, replace with your own
+struct DATA {
+  unsigned long Count;
+  int Bits;
+  float Volts;
+  float Amps;
+
+};
+
+int Chan;
+DATA MyData;
+int i;
+
+// you will need to define the pins to create the serial port
+SoftwareSerial ESerial(PIN_RX, PIN_TX);
+
+// create the transceiver object, passing in the serial and pins
+EBYTE Transceiver(&ESerial, PIN_M0, PIN_M1, PIN_AX);
+
+void setup() {
+
+  Serial.begin(9600);
+
+  ESerial.begin(9600);
+  
+  Serial.println("Starting Sender");
+
+  // this init will set the pinModes for you
+  Serial.println(Transceiver.init());
+
+  // all these calls are optional but shown to give examples of what you can do
+
+  // Serial.println(Transceiver.GetAirDataRate());
+  // Serial.println(Transceiver.GetChannel());
+  // Transceiver.SetAddressH(1);
+  // Transceiver.SetAddressL(1);
+  // Chan = 15;
+  // Transceiver.SetChannel(Chan);
+  // save the parameters to the unit,
+  // Transceiver.SaveParameters(PERMANENT);
+
+  // you can print all parameters and is good for debugging
+  // if your units will not communicate, print the parameters
+  // for both sender and receiver and make sure air rates, channel
+  // and address is the same
+  Transceiver.PrintParameters();
+
+}
+
+void loop() {
+
+  // measure some data and save to the structure
+  MyData.Count++;
+  MyData.Bits = analogRead(A0);
+  MyData.Volts = MyData.Bits * ( 5.0 / 1024.0 );
+
+  // i highly suggest you send data using structures and not
+  // a parsed data--i've always had a hard time getting reliable data using
+  // a parsing method
+  Transceiver.SendStruct(&MyData, sizeof(MyData));
+
+    // note, you only really need this library to program these EBYTE units
+    // you can call write directly on the EBYTE Serial object
+    // ESerial.write((uint8_t*) &Data, PacketSize );
+
+
+  // let the use know something was sent
+  Serial.print("Sending: "); Serial.println(MyData.Count);
+  delay(1000);
+
+
+}
